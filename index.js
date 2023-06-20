4fffd0eaa23485f1da7e9bc7e2b68f65e19fd871
Your Name <gms441@mail.ru>
2023-06-20 21:32:09 +0700

    добавлены группы и уменьшен интервал постинга

--- index.js
+++ index.js
@@ -7,60 +7,65 @@ const axios = require("axios");
 import config from './config.js';
 
 const bot = new Telegraf(config.tg_token);
-const intervalMs = 10800000;  // отправляем картинки раз в 3 часа
+const intervalMs = 3600000;  // 1 час
 
-let lastImageId = null;
+const apiTypes = {
+    cherry: {
+        url: `https://api.vk.com/method/wall.get?owner_id=-${config.vk_app_cherry}&count=2&access_token=${config.vk_service_key}&v=${config.vk_ver}`,
+        lastId: null
+    },
+
+    geek: {
+        url: `https://api.vk.com/method/wall.get?owner_id=-${config.vk_app_geek}&count=2&access_token=${config.vk_service_key}&v=${config.vk_ver}`,
+        lastId: null
+    },
+
+    asian: {
+        url: `https://api.vk.com/method/wall.get?owner_id=-${config.vk_app_asian}&count=2&access_token=${config.vk_service_key}&v=${config.vk_ver}`,
+        lastId: null
+    }
+};
 
 async function run() {
-    // блок с первым постингом
-    const vkPostUrl = `https://api.vk.com/method/wall.get?owner_id=-${config.vk_app}&count=2&access_token=${config.vk_service_key}&v=${config.vk_ver}`;
-    axios.get(vkPostUrl)
-        .then(response => {
-            try {
-                const imageId = response.data.response.items[0].id;
-
-                // не отправляем одинаковые посты
-                if(imageId !== lastImageId) { 
-                    lastImageId = imageId;
-
-                    const imageList = response.data.response.items[0].attachments; 
-
-                    // обрезаем лишние теги картинки
-                    const imageTags = response.data.response.items[0].text.match(/#[^\s]+/g);
-                    const imageTagsParsed = imageTags.map(hashtag => hashtag.replace(/@\S+/, '')).map(hashtag => hashtag.replace(/#[а-яА-Я]+/, ''));
-                    const imageTagsString = imageTagsParsed.join(' ');
-                    // конец блока
-
-                    const photoList = [];
-
-                    // если картинок несколько в посте, отправляем все одним сообщением
-                    imageList.forEach(image => {
-                        const imageSizes = image.photo.sizes; 
-                        const imageUrl = imageSizes[imageSizes.length - 1];
-                        photoList.push({
-                            type: 'photo',
-                            media: imageUrl
-                        });
-                    });
-
-                    // добавляем описание
-                    photoList[0].caption = imageTagsString;
-
-                    bot.telegram.sendMediaGroup(config.tg_group_cherry, photoList);
-                }
+    let isPost = false;
+    for (let key in apiTypes) {
+        const post = await axios.get(apiTypes[key].url).then(response => {
+            const imageId = response.data.response.items[0].id;
+            if(imageId !== apiTypes[key].lastId) {
+                const payload = getContent(response);
+                apiTypes[key].lastId = imageId;
+                isPost = true;
+                bot.telegram.sendMediaGroup(config.tg_group_cherry, payload);
             }
-            catch(e) {
-                console.log(e);
-            }
-        })
-        .catch(error => {
-            console.log(error)
         });
-
-        // TODO // блок со вторым постингом
-        // ..............................
+        if(isPost) break;
+    }
 
     setTimeout(run, intervalMs);
+};
+
+const getContent = (response) => {
+    const imageId = response.data.response.items[0].id;
+    const imageList = response.data.response.items[0].attachments; 
+
+    // обрезаем лишние теги картинки
+    const imageTags = response.data.response.items[0].text.match(/#[^\s]+/g);
+    const imageTagsParsed = imageTags.map(hashtag => hashtag.replace(/@\S+/, '')).map(hashtag => hashtag.replace(/#[а-яА-Я]+/, ''));
+    const imageTagsString = imageTagsParsed.join(' ');
+
+    const photoList = [];
+
+    imageList.forEach(image => {
+        const imageSizes = image.photo.sizes; 
+        const imageUrl = imageSizes[imageSizes.length - 1];
+        photoList.push({
+            type: 'photo',
+            media: imageUrl
+        });
+    });
+
+    photoList[0].caption = imageTagsString;
+    return photoList;
 };
 
 run();
