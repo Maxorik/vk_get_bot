b1aa94a313e5ffb9f8d1a29a4de26311debaf411
Your Name <gms441@mail.ru>
2023-07-03 20:31:12 +0700

    добавлена возможность показывать одну либо все картинки с поста

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import config from './config.js';
 
 const bot = new Telegraf(config.tg_token);
 const intervalMs = 3600000;  // 1 час
+const getAllImagesInArray = false; // true если хотим получить ВСЕ изображения из поста, false если одно
 
 const apiTypes = {
     cherry: {
@@ -33,59 +34,75 @@ async function run() {
     let isPost = false; // постили ли контент в этот интервал
 
     for (let key in apiTypes) {
-        const post = await axios.get(apiTypes[key].url).then(response => {
-            const imageId = response.data.response.items[0].id;
+        try {
+            const post = await axios.get(apiTypes[key].url).then(response => {
+                const position = response.data.response.items[0].is_pinned ? 1 : 0; // закрепленный пост нам не нужен
+                const imageId = response.data.response.items[position].id;
 
-            // если пост еще не был ранее опубликован, получаем его содержимое
-            if(imageId !== apiTypes[key].lastId) {
-                const payload = getContent(response);
-                apiTypes[key].lastId = imageId;
+                // если пост еще не был ранее опубликован, получаем его содержимое
+                if(imageId !== apiTypes[key].lastId) {
+                    const payload = getContent(response, position);
+                    apiTypes[key].lastId = imageId;
 
-                // если в этот интервал публикация уже была, запоминаем контент, чтобы выслать его в случае простоя
-                if (isPost) {
-                    if (reservContent.length < 20) {
-                        const isReserved = reservContent.find(item => item[0].media === payload[0].media);
-                        !isReserved && reservContent.push(payload);
-                    };
-                } 
+                    // если в этот интервал публикация уже была, запоминаем контент, чтобы выслать его в случае простоя
+                    if (isPost) {
+                        if (reservContent.length < 20) {
+                            const isReserved = reservContent.find(item => item[0].media === payload[0].media);
+                            !isReserved && reservContent.push(payload);
+                        };
+                    } 
 
-                // публикуем, если контент уникальный
-                else {
-                    isPost = true;
-                    bot.telegram.sendMediaGroup(config.tg_group_cherry, payload);
+                    // публикуем, если контент уникальный
+                    else {
+                        isPost = true;
+                        bot.telegram.sendMediaGroup(config.tg_group_cherry, payload);
+                    }
                 }
-            }
-        });
-    };
+            });
+        } catch (error) {
+            bot.telegram.sendMessage(config.tg_my_id, error);
+        }
+    }
 
     // простой постинга - берем контент из резерва
     if (!isPost && reservContent.length > 0) {
         bot.telegram.sendMediaGroup(config.tg_group_cherry, reservContent[0]);
         reservContent.shift();
-    };
+    }
 
     setTimeout(run, intervalMs);
-};
+}
 
-const getContent = (response) => {
-    const imageId = response.data.response.items[0].id;
-    const imageList = response.data.response.items[0].attachments; 
+const getContent = (response, position) => {
+    const imageProps = response.data.response.items[position];
+    const imageList = imageProps.attachments;
 
     // обрезаем лишние теги картинки
-    const imageTags = response.data.response.items[0].text.match(/#[^\s]+/g);
+    const imageTags = imageProps.text.match(/#[^\s]+/g);
     const imageTagsParsed = imageTags.map(hashtag => hashtag.replace(/@\S+/, '')).map(hashtag => hashtag.replace(/#[а-яА-Я]+/, ''));
     const imageTagsString = imageTagsParsed.join(' ');
 
     const photoList = [];
 
-    imageList.forEach(image => {
-        const imageSizes = image.photo.sizes; 
+    if (getAllImagesInArray) {
+        imageList.forEach(image => {
+            const imageSizes = image.photo.sizes;
+            const imageUrl = imageSizes[imageSizes.length - 1];
+            photoList.push({
+                type: 'photo',
+                media: imageUrl
+            });
+        });
+    } else {
+        // возвращаем случайную картинку из группы
+        const randomImage = imageList[Math.floor(Math.random() * Math.floor(imageList.length))];
+        const imageSizes = randomImage.photo.sizes;
         const imageUrl = imageSizes[imageSizes.length - 1];
         photoList.push({
             type: 'photo',
             media: imageUrl
         });
-    });
+    }
 
     photoList[0].caption = imageTagsString;
     return photoList;
